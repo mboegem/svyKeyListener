beb8a2b47c227ff5fea5cd71abf41b9341d802c4
tuanway <tuan@atndev.com>
2020-10-06 17:42:28 -0500

    * add delay parameter * add a regex pattern restriction for client side removal of keystroke

--- keylistener/keylistener/keylistener.js
+++ keylistener/keylistener/keylistener.js
@@ -1,7 +1,6 @@
 angular.module('keyListener', ['servoy']).factory("keyListener", function($services, $window, $timeout, $log) {
 		var scope = $services.getServiceScope('keyListener');
-		
-		
+
 		function removeKeyListener(callbackKey) {
 			var isRemoved = false;
 			// it may remove more than one element
@@ -13,22 +12,25 @@ angular.module('keyListener', ['servoy']).factory("keyListener", function($servi
 					isRemoved = true;
 				}
 			}
-			return isRemoved ;
+			return isRemoved;
 		}
-		
+
 		return {
-			addKeyListener: function(callbackKey, callback, clearCB) {
+			addKeyListener: function(callbackKey, callback, clearCB, delay, restrictPattern) {
 				if (!scope.model.callbacks) scope.model.callbacks = [];
 				if (clearCB) {
 					removeKeyListener(callbackKey);
 				}
 				if (!scope.model.callbacks) scope.model.callbacks = [];
-				scope.model.callbacks.push({ 'callbackKey': callbackKey, 'callback': callback });
+				if (!delay) {
+					delay = 5000;
+				}
+				scope.model.callbacks.push({ 'callbackKey': callbackKey, 'callback': callback, 'delay': delay, 'restrictPattern': restrictPattern, 'isRunning': false });
 			},
 			getCallback: function(callbackKey) {
 				for (var i = 0; scope.model.callbacks && i < scope.model.callbacks.length; i++) {
 					var c = scope.model.callbacks[i];
-					if (c.callbackKey == callbackKey) return c.callback;
+					if (c.callbackKey == callbackKey) return c;
 				}
 				return null;
 			},
@@ -44,18 +46,20 @@ angular.module('keyListener', ['servoy']).factory("keyListener", function($servi
 					handleKeyEvent(event);
 				}),
 				$element.keydown(function(event) {
-					if (event.keyCode === 20 && navigator.appVersion.indexOf("Mac")!=-1) {
+					if (event.keyCode === 20 && navigator.appVersion.indexOf("Mac") != -1) {
 						// handle caps lock keyevent exceptions on Mac:
 						// keydown is triggered in case of toggle to ON in Safari/Chrome, ON/OFF in Firefox
 						// Windows does handle both ON/OFF in the keyup event
-						
 						handleKeyEvent(event);
 					}
 				})
-				
+
 				function handleKeyEvent(event) {
-					var callback = keyListener.getCallback($attrs.keylistener);
+					var cb = keyListener.getCallback($attrs.keylistener);
+					var callback = cb.callback
 					if (callback) {
+						if (callback.isRunning) return;
+						callback.isRunning = true;
 						var input;
 						var value;
 						var capsLockEnabled = false;
@@ -70,17 +74,33 @@ angular.module('keyListener', ['servoy']).factory("keyListener", function($servi
 								input = $element;
 							}
 						}
-						
-						if(event.getModifierState) {
+
+						if (event.getModifierState) {
 							capsLockEnabled = event.getModifierState("CapsLock")
 						} else {
 							var oriEvent = event.originalEvent;
-							if(oriEvent.getModifierState) {
+							if (oriEvent.getModifierState) {
 								capsLockEnabled = oriEvent.getModifierState("CapsLock");
 							}
 						}
-					    var jsEvent = $utils.createJSEvent(event, 'action'); 
-						$window.executeInlineScript(callback.formname, callback.script, [input.val(), jsEvent, event.keyCode, event.altKey, event.ctrlKey, event.shiftKey, capsLockEnabled]);
+						var jsEvent = $utils.createJSEvent(event, 'action');
+						setTimeout(function() {
+								$window.executeInlineScript(callback.formname, callback.script, [input.val(), jsEvent, event.keyCode, event.altKey, event.ctrlKey, event.shiftKey, capsLockEnabled]);
+								callback.isRunning = false;
+								var restrictPattern = cb.restrictPattern;
+								//if there is a restriction on the pattern, remove last typed character in the event if not matching.
+								if (restrictPattern) {
+									var reg = new RegExp(cb.restrictPattern);
+									event.currentTarget.value = event.currentTarget.value.replaceAll(reg, '');
+									// needed as some browsers will use autocomplete
+									setTimeout(function() {
+											event.currentTarget.value = event.currentTarget.value.replaceAll(reg, '');
+										}, 500)
+									setTimeout(function() {
+											event.currentTarget.value = event.currentTarget.value.replaceAll(reg, '');
+										}, 1000)
+								}
+							}, callback.delay)
 					}
 				}
 			}
